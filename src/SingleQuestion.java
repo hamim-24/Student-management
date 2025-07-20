11634e6b151a714e15786ec8d78bdf1ab57d477a
hamim <hamim-24>
2025-07-21 00:54:00 +0600

    update ui

--- src/SingleQuestion.java
+++ src/SingleQuestion.java
@@ -12,7 +12,7 @@ public class SingleQuestion {
 
     public String toString() {
         return String.format(
-                "* %s\n a) %s\n b) %s\n c)%s\n d)%s\n", question, options[0], options[1], options[2], options[3]
+                "%s\n    a) %s\n    b) %s\n    c) %s\n    d) %s\n", question, options[0], options[1], options[2], options[3]
         );
     }
 }
